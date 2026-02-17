884bdd3e5d1ecb0fb57eb7e393974dd01ce49886
NUMAN GURBUZ <numangurbuz11@hotmail.com>
2026-02-17 11:29:30 +0300

    Initial commit - React TS project

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <h1>WEB TASARIM VE PROGRAMLAMA</h1>
+        <h2>LAB-1</h2>
+          <p>AD SOYAD :numan gürbüz </p>
+       <p> Ogrenci No:: 230542012</p>
+        
+      
+      </div>
+      
+      
+    
+    </>
+  )
+}
+
+export default App
